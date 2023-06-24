42ceadd6919c5fd4a3fc4a72c240e6ed3e2ad75a
Gergana <onyxblack2016@gmail.com>
2023-06-24 14:23:21 +0300

    flexbox grid responsive gallery #2

--- src/Container/index.js
+++ src/Container/index.js
@@ -4,53 +4,44 @@ function Container() {
    return (
       <>
          <Gutter className="grid gutters">
-            <div className="cell">
+            <div className="half">
                <div>
-                  Quis duis officia nostrud ullamco elit Lorem velit esse consequat magna ut cupidatat mollit.
+                  Est ea esse magna exercitation cupidatat nostrud nostrud est fugiat commodo velit amet.
                </div>
             </div>
-            <div className="cell">
+            <div className="half">
                <div>
-                  Quis duis officia nostrud ullamco elit Lorem velit
-               </div>
-            </div>
-            <div className="cell">
-               <div>
-                  Quis duis officia nostrud ullamco elit Lorem velit esse consequat magna ut cupidatat mollit.
+                  Eu Lorem consectetur eu excepteur ipsum proident irure ut nulla anim non reprehenderit dolore laboris.
                </div>
             </div>
          </Gutter>
          <Gutter className="grid gutters">
-            <div className="cell large">
-               <div>
-                  Quis duis officia nostrud ullamco elit Lorem velit esse consequat magna ut cupidatat mollit.
-               </div>
+            <div className="third"><div>Occaecat qui fugiat aliquip ex ullamco culpa ex incididunt et minim reprehenderit.</div>
             </div>
-            <div className="cell">
-               <div>
-                  Quis duis officia nostrud ullamco elit Lorem velit
-               </div>
+            <div className="third"><div>Consequat nisi laboris cupidatat ad irure</div>
+            </div>
+            <div className="third"><div>Veniam deserunt veniam ad pariatur officia aliqua enim incididunt nostrud velit Lorem.
+               Quis velit officia ullamco id dolor eu consectetur dolore ea.
+               Cupidatat ea ad dolore cillum elit cillum duis ullamco voluptate irure eu ad minim aliqua.</div>
             </div>
          </Gutter>
          <Gutter className="grid gutters">
-            <div className="cell">
+            <div className="sixth">
+               <div>Occaecat ad reprehenderit proident minim consequat officia.</div>
+            </div>
+            <div className="sixth">
                <div>
-                  Quis duis officia nostrud ullamco elit Lorem velit esse consequat magna ut cupidatat mollit.
+                  Ipsum aliqua sit tempor sunt id qui sit exercitation elit ea nisi excepteur reprehenderit.
                </div>
             </div>
-            <div className="cell">
+            <div className="third">
                <div>
-                  Quis duis officia nostrud ullamco elit Lorem velit
+                  Ea Lorem laboris proident ipsum ad sint nulla sunt ad tempor quis eu dolor.
                </div>
             </div>
-            <div className="cell">
+            <div className="third">
                <div>
-                  Quis duis officia nostrud ullamco elit Lorem velit esse consequat magna ut cupidatat mollit.
-               </div>
-            </div>
-            <div className="cell">
-               <div>
-                  Quis duis officia nostrud ullamco elit Lorem velit
+                  Ea Lorem laboris proident ipsum ad sint nulla sunt ad tempor quis eu dolor.
                </div>
             </div>
          </Gutter>
